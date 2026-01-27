0b36efce9c5c4c187ec9343e28fe7b69ea7c908c
Cherry <aazetthr@gmail.com>
2026-01-27 17:49:39 +0300

    chore: fix requires

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import type OsuNativeType from "osu-native";
 let addon: typeof OsuNativeType;
 
 try {
-  addon = require("./dist/osu_native_napi.node");
+  addon = require("./dist/osu-native-napi.node");
 } catch {
-  addon = require("./dist/Release/osu_native_napi.node");
+  addon = require("./dist/Release/osu-native-napi.node");
 }
 
 export default addon;
