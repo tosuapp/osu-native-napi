e41b51a25860bb312d2431b5308dc134ea50bcea
Mikhail Babynichev <i@kotrik.ru>
2026-01-27 23:11:14 +0300

    chore: npm friendly package

---
+++ packages/cpp-binding/index.mjs
@@ -0,0 +1,4 @@
+import addon from "./dist/Release/osu-native-napi.node";
+
+export default addon;
+export { addon };
--- packages/cpp-binding/index.ts
+++ packages/cpp-binding/index.ts
@@ -1,12 +1,4 @@
 import type * as OsuNativeType from "./osu-native";
 
-let addon: typeof OsuNativeType;
-
-try {
-  addon = require("./dist/osu-native-napi.node");
-} catch {
-  addon = require("./dist/Release/osu-native-napi.node");
-}
-
-export default addon;
+export default require("./dist/Release/osu-native-napi.node") as typeof OsuNativeType
 export type * from "./osu-native";
