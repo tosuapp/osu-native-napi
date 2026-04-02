551e7d0bef0e7a91caaecbcffaf95c53f6d8cf1d
ck <21735205+cyperdark@users.noreply.github.com>
2026-04-02 22:29:31 +0300

    use different path

--- packages/cpp-binding/index.js
+++ packages/cpp-binding/index.js
@@ -2,7 +2,7 @@
 
 let file_path = '';
 
-if (process.platform == 'win32') file_path = "./dist/Release/osu-native-napi-win-x64.node";
-else if (process.platform == 'linux') file_path = "./dist/Release/osu-native-napi-linux-x64.node";
+if (process.platform == 'win32') file_path = "./dist/osu-native-napi-win-x64.node";
+else if (process.platform == 'linux') file_path = "./dist/osu-native-napi-linux-x64.node";
 
 module.exports = require(file_path);
